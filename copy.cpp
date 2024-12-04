7c1e9f25b5cefcc7a285e716ce20892504e664d8
AMM-222 <amm222426@gmail.com>
2024-12-04 10:14:14 +0500

    Add files via upload

---
+++ copy.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+	char source[]= "hello world";
+	char destination[50];
+	strcpy(destination,source);
+	cout<<"source string: "<<source<<endl;
+	cout<<"destination string: "<<destination<<endl;
+	
+	
+
+	
+}
---
+++ lenght.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+	string str1="hello world";
+	int length=str1.length();
+	cout<<"lenght of the string is"<<length<<endl;
+	
+}
